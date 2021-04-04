c5593d5b8cc12465f42c1fd49a92d6ab06c77bc5
Do Thi Hai Van <annie.do247@gmail.com>
2021-04-04 21:28:32 +0700

    add user role as staff

--- models/User.js
+++ models/User.js
@@ -8,7 +8,7 @@ const userSchema = Schema(
     name: { type: String, required: true , trim: true},
     email: { type: String, required: true, unique: true, trim: true, lowercase : true },
     password: { type: String, required: true },
-    role: { type: String, enum: ["user", "admin"], default: "user" },
+    role: { type: String, enum: ["user", "admin","staff"], default: "user" },
     avatarUrl:String,
     favorite:[{ type: Schema.Types.ObjectId, ref: "Product" }],
     balance: { type: Number, default: 0 },
